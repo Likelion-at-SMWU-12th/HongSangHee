012dbd996869b30579f13303e884401b4d28d4ea
SangHee-Hong <hongdabagi@gmail.com>
2024-07-08 19:08:53 +0900

    회원가입 및 로그인 api 만들기

--- week06/accounts/urls.py
+++ week06/accounts/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from .views import signup_view, login_view, logout_view
+from .views import signup_api, login_api
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', signup_view, name = 'signup'),
-    path('login/', login_view, name = 'login'),
+    #path('signup/', signup_view, name = 'signup'),
+    #path('login/', login_view, name = 'login'),
     path('logout/', logout_view, name = 'logout'),
+    path('signup/', signup_api, name='signup'),
+    path('login/', login_api, name='login'),
 
 
 ]
