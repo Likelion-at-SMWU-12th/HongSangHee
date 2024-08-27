9355c3d91a4c18bd6d3eab52eced7f063ec82699
SangHee-Hong <hongdabagi@gmail.com>
2024-08-27 18:37:39 +0900

    저번 세미나

---
+++ crud/src/main/java/hongsanghee/crud/Controller/GetController.java
@@ -0,0 +1,45 @@
+package hongsanghee.crud.Controller;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+
+@RestController
+ @RequestMapping("/api/v1/get-api")
+public class GetController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+     public String getHello(){
+        return "Hello World";
+    }
+
+    @GetMapping(value = "/name")
+    public String getName(){
+            return "Flature";
+    }
+    @GetMapping(value = "/variable1/{variable}")
+     public String getVariablie1(@PathVariable String variable){
+         return variable;
+     }
+     @GetMapping(value = "/request1")
+     public String getRequestParam1(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization){
+        return name + " " + email + " " +organization;
+     }
+
+     @GetMapping(value = "/request2")
+     public String getRequestParam2(@RequestParam Map<String, String> param){
+        StringBuilder sb = new StringBuilder();
+
+        param.entrySet().forEach(map ->{
+            sb.append(map.getKey()+" : "+ map.getValue()+"\n");
+        });
+        return sb.toString();
+
+     }
+
+}
+
+
+
