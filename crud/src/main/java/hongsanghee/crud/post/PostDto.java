0c6a35ba892e43403270b5f4632a97cc7c116509
SangHee-Hong <hongdabagi@gmail.com>
2024-09-02 01:22:21 +0900

    [Feat]스프링 3주차 세미나 실습

--- crud/src/main/java/hongsanghee/crud/post/PostDto.java
+++ crud/src/main/java/hongsanghee/crud/post/PostDto.java
@@ -1,45 +1,67 @@
 package hongsanghee.crud.post;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class PostDto {
+    private int id;
     private String title;
     private String content;
     private String writer;
+    private int boardId;
 
-    public PostDto(String title, String content,String writer) {
+    public PostDto(int id, String title, String content, String writer, int boardId) {
+        this.id = id;
         this.title = title;
         this.content = content;
         this.writer = writer;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getWriter() {
-        return writer;
-    }
-
-    public void setWriter(String writer) {
-        this.writer = writer;
-    }
-
-    @Override
-    public String toString(){
-        return "PostDto{"+
-                "title="+title+'\''+
-                ",content="+content+'\''+
-                "}";
+        this.boardId = boardId;
     }
 }
+//public class PostDto {
+//    private String title;
+//    private String content;
+//    private String writer;
+//
+//    public PostDto(String title, String content,String writer) {
+//        this.title = title;
+//        this.content = content;
+//        this.writer = writer;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//
+//    public String getWriter() {
+//        return writer;
+//    }
+//
+//    public void setWriter(String writer) {
+//        this.writer = writer;
+//    }
+//
+//    @Override
+//    public String toString(){
+//        return "PostDto{"+
+//                "title="+title+'\''+
+//                ",content="+content+'\''+
+//                "}";
+//    }
+//}
--- crud/src/main/java/hongsanghee/crud/post/PostRestController.java
+++ crud/src/main/java/hongsanghee/crud/post/PostRestController.java
@@ -1,64 +1,64 @@
-package hongsanghee.crud.post;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
-import java.util.List;
-
-@RestController
-@RequestMapping("post")
-public class PostRestController{
-    private static final Logger logger= LoggerFactory.getLogger(PostRestController.class);
-    private final List<PostDto> postList;
-    public PostRestController() {
-        this.postList = new ArrayList<>();
-    }
-
-    // 1 createPost
-    // http://localhost:8080/post
-    // POST /post
-
-    @PostMapping()
-    public void createPost(@RequestBody PostDto postDto){
-        logger.info(postDto.toString());
-        this.postList.add(postDto);
-    }
-
-    // 2 get
-    // http://localhost:8080/post
-    // GET /post
-    @GetMapping()
-    public List<PostDto> readPostAll(){
-        logger.info("iin read post all");
-        return this.postList;
-    }
-
-    // GET /post/0/
-    // GET /post?id=0/
-    @GetMapping("{id}")
-    public PostDto readPost(@PathVariable("id") int id){
-        logger.info("in read post");
-        return this.postList.get(id);
-    }
-
-    //수정하는 RESTFUL API 설계하기
-    // PUT /post?id=0처럼 하면 되겠죠..?
-    @PutMapping("{id}")
-    public void updatePost(@PathVariable("id") int id, @RequestBody PostDto postDto){
-        logger.info("in update post");//로그 남기기
-        PostDto Post = this.postList.get(id);
-        if (Post != null){
-            Post.setTitle(postDto.getTitle());
-            Post.setContent(postDto.getContent());
-            Post.setWriter(postDto.getWriter());
-        }
-
-    }
-    // Delete하는 RESTFUL API 설계하기
-    // DELETE /post?id=0/
-    @DeleteMapping("{id}")
-    public void deletePost(@PathVariable("id") int id){
-        logger.info("in delete post");
-        this.postList.remove(id);
-    }
-}
+//package hongsanghee.crud.post;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.web.bind.annotation.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("post")
+//public class PostRestController{
+//    private static final Logger logger= LoggerFactory.getLogger(PostRestController.class);
+//    private final List<PostDto> postList;
+//    public PostRestController() {
+//        this.postList = new ArrayList<>();
+//    }
+//
+//    // 1 createPost
+//    // http://localhost:8080/post
+//    // POST /post
+//
+//    @PostMapping()
+//    public void createPost(@RequestBody PostDto postDto){
+//        logger.info(postDto.toString());
+//        this.postList.add(postDto);
+//    }
+//
+//    // 2 get
+//    // http://localhost:8080/post
+//    // GET /post
+//    @GetMapping()
+//    public List<PostDto> readPostAll(){
+//        logger.info("iin read post all");
+//        return this.postList;
+//    }
+//
+//    // GET /post/0/
+//    // GET /post?id=0/
+//    @GetMapping("{id}")
+//    public PostDto readPost(@PathVariable("id") int id){
+//        logger.info("in read post");
+//        return this.postList.get(id);
+//    }
+//
+//    //수정하는 RESTFUL API 설계하기
+//    // PUT /post?id=0처럼 하면 되겠죠..?
+//    @PutMapping("{id}")
+//    public void updatePost(@PathVariable("id") int id, @RequestBody PostDto postDto){
+//        logger.info("in update post");//로그 남기기
+//        PostDto Post = this.postList.get(id);
+//        if (Post != null){
+//            Post.setTitle(postDto.getTitle());
+//            Post.setContent(postDto.getContent());
+//            Post.setWriter(postDto.getWriter());
+//        }
+//
+//    }
+//    // Delete하는 RESTFUL API 설계하기
+//    // DELETE /post?id=0/
+//    @DeleteMapping("{id}")
+//    public void deletePost(@PathVariable("id") int id){
+//        logger.info("in delete post");
+//        this.postList.remove(id);
+//    }
+//}
