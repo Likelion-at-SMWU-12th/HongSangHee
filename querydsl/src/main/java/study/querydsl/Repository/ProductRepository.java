181e74dbb8a91fc85911daabd7809b1a5519a0a0
SangHee-Hong <hongdabagi@gmail.com>
2024-09-09 18:19:05 +0900

    [Feat]Repository에 인기상품 top10 반환 메서드 추가

--- querydsl/src/main/java/study/querydsl/Repository/ProductRepository.java
+++ querydsl/src/main/java/study/querydsl/Repository/ProductRepository.java
@@ -4,10 +4,14 @@ import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import study.querydsl.entity.Product;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    // JPA를 활용하여 쿼리 메소드를 통해 인기도 TOP 10 반환하기
+
+    @Query("SELECT p FROM Product p ORDER BY p.popularity DESC")
+    List<Product> findTop10ByPopularity();
+
 }
