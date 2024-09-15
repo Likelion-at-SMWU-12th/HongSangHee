e344143efc4b4c695d6b058810776af3e24a3be3
SangHee-Hong <hongdabagi@gmail.com>
2024-09-15 19:58:36 +0900

    [Feat]CommentRepository 작성

---
+++ crud/src/main/java/hongsanghee/crud/respository/CommentRepository.java
@@ -0,0 +1,7 @@
+package hongsanghee.crud.respository;
+
+import hongsanghee.crud.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
