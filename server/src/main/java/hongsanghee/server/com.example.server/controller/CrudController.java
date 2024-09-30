48990dfccccff0bc1e033fb7ea90909f02382499
SangHee-Hong <hongdabagi@gmail.com>
2024-09-30 18:04:00 +0900

    RestTemplate과 WebClient 구현

---
+++ server/src/main/java/hongsanghee/server/com.example.server/controller/CrudController.java
@@ -0,0 +1,51 @@
+package hongsanghee.server.com.example.server.controller;
+
+
+import hongsanghee.server.com.example.server.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/crud-api")
+public class CrudController {
+
+    @GetMapping
+    public String getName(){
+        return "Judy";
+    }
+
+    @GetMapping("/{variable}")
+    public String getValue(@PathVariable String variable){
+        return variable;
+    }
+
+    @GetMapping("/param")
+    public String getNameWithParam(@RequestParam String name){
+        return "Hello. " + name + "!";
+    }
+
+    @PostMapping
+    public ResponseEntity<MemberDto> getMember(
+            @RequestBody MemberDto request,
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization
+    ) {
+        MemberDto memberDto = new MemberDto();
+        memberDto.setName(name);
+        memberDto.setEmail(email);
+        memberDto.setOrganization(organization);
+        return ResponseEntity.status(HttpStatus.OK).body(memberDto);
+    }
+
+    // POST '/api/v1/crud-api/add-header
+    // 요청 헤더 my-header와 본문 MemberDto를 받아서 MemberDto 객체 반환
+    @PostMapping("/add-header")
+    public ResponseEntity<MemberDto> addHeader(@RequestHeader("my-header") String header,
+                                               @RequestBody MemberDto memberDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(memberDto);
+    }
+
+
+}
